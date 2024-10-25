b795188bd9b9adbed34bf1a28e361036ba475426
Shuhei Matsumoto <smatsumoto@nvidia.com>
2024-10-25 03:10:52 +0000

    bdev/nvme: Factor out operations under mutex from bdev_nvme_reset_ctrlr()
    Factor out operations under mutex held from bdev_nvme_reset_ctrlr()
    into bdev_nvme_reset_ctrlr_unsafe().
    
    We want to move pending reset list from per SPDK thread to global, and
    then move enqueue operation into bdev_nvme_reset_ctrlr().
    
    However, bdev_nvme_reset_ctrlr() is shared between JSON RPC and bdev_io.
    To keep code cleanness, this refactoring is necessary.
    
    Signed-off-by: Shuhei Matsumoto <smatsumoto@nvidia.com>

--- module/bdev/nvme/bdev_nvme.c
+++ module/bdev/nvme/bdev_nvme.c
@@ -2316,20 +2316,16 @@ _bdev_nvme_reset(void *ctx)
 }
 
 static int
-bdev_nvme_reset(struct nvme_ctrlr *nvme_ctrlr)
+bdev_nvme_reset_unsafe(struct nvme_ctrlr *nvme_ctrlr, spdk_msg_fn *msg_fn)
 {
 	struct nvme_path_id *path_id;
-	spdk_msg_fn msg_fn;
 
-	pthread_mutex_lock(&nvme_ctrlr->mutex);
 	if (nvme_ctrlr->destruct) {
-		pthread_mutex_unlock(&nvme_ctrlr->mutex);
 		NVME_CTRLR_NOTICELOG(nvme_ctrlr, "Unable to perform reset, being deleted.\n");
 		return -ENXIO;
 	}
 
 	if (nvme_ctrlr->resetting) {
-		pthread_mutex_unlock(&nvme_ctrlr->mutex);
 		NVME_CTRLR_NOTICELOG(nvme_ctrlr, "Unable to perform reset, already in progress.\n");
 		return -EBUSY;
 	}
@@ -2339,10 +2335,10 @@ bdev_nvme_reset(struct nvme_ctrlr *nvme_ctrlr)
 
 	if (nvme_ctrlr->reconnect_is_delayed) {
 		NVME_CTRLR_NOTICELOG(nvme_ctrlr, "Reconnect is already scheduled.\n");
-		msg_fn = _bdev_nvme_reconnect;
+		*msg_fn = _bdev_nvme_reconnect;
 		nvme_ctrlr->reconnect_is_delayed = false;
 	} else {
-		msg_fn = _bdev_nvme_reset;
+		*msg_fn = _bdev_nvme_reset;
 		assert(nvme_ctrlr->reset_start_tsc == 0);
 	}
 
@@ -2355,10 +2351,24 @@ bdev_nvme_reset(struct nvme_ctrlr *nvme_ctrlr)
 		path_id->is_failed = false;
 	}
 
+	return 0;
+}
+
+static int
+bdev_nvme_reset(struct nvme_ctrlr *nvme_ctrlr)
+{
+	spdk_msg_fn msg_fn;
+	int rc;
+
+	pthread_mutex_lock(&nvme_ctrlr->mutex);
+	rc = bdev_nvme_reset_unsafe(nvme_ctrlr, &msg_fn);
 	pthread_mutex_unlock(&nvme_ctrlr->mutex);
 
-	spdk_thread_send_msg(nvme_ctrlr->thread, msg_fn, nvme_ctrlr);
-	return 0;
+	if (rc == 0) {
+		spdk_thread_send_msg(nvme_ctrlr->thread, msg_fn, nvme_ctrlr);
+	}
+
+	return rc;
 }
 
 int
