spdk/spdk

lib/rdma_provider: free memory pointed by mlx5_qp->common.stats
When creating mlx5 qpair fails, free the memory pointed to by
mlx5_qp->common.stats.

Change-Id: I289033c128961e2636e4691615c0c7fdd45f7ea8
Signed-off-by: Yankun Li <yankun@staff.sina.com>
Reviewed-on: https://review.spdk.io/c/spdk/spdk/+/26103
Community-CI: Mellanox Build Bot
Reviewed-by: Jim Harris <jim.harris@nvidia.com>
Reviewed-by: Konrad Sztyber <ksztyber@nvidia.com>
Reviewed-by: Aleksey Marchuk <alexeymar@nvidia.com>
Tested-by: SPDK Automated Test System <spdkbot@gmail.com>

lib/rdma_provider/rdma_provider_mlx5_dv.c
@@ -116,6 +116,9 @@ spdk_rdma_provider_qp_create(struct rdma_cm_id *cm_id,
 
 	if (!qp) {
 		SPDK_ERRLOG("Failed to create qpair, errno %s (%d)\n", spdk_strerror(errno), errno);
+		if (!mlx5_qp->common.shared_stats) {
+			free(mlx5_qp->common.stats);
+		}
 		free(mlx5_qp);
 		return NULL;
 	}

andrii-holovchenko/spdk-mellanox

lib/rdma_provider/rdma_provider_mlx5_dv.c
@@ -91,6 +91,9 @@ spdk_rdma_provider_qp_create(struct rdma_cm_id *cm_id,
 	rc = spdk_mlx5_qp_create(pd, dv_cq->mlx5_cq, &mlx5_qp_attr, &dv_qp->mlx5_qp);
 	if (rc) {
 		SPDK_ERRLOG("Failed to create qpair, rc %d\n", rc);
+		if (!dv_qp->common.shared_stats) {
+			free(dv_qp->common.stats);
+		}
 		free(dv_qp);
 		return NULL;
 	}